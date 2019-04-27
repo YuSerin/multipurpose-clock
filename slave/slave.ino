247427ed176b229b8552e6187f428c0d94861e07
Aaron Roberts <lordgankoo@gmail.com>
2019-04-27 13:41:28 -0600

    Segment displays time

--- slave/slave.ino
+++ slave/slave.ino
@@ -7,7 +7,10 @@
 enum cmd {set_time, set_alarm, set_timer, cmd_up, cmd_down};
 SevSeg sevseg;
 
-void setup() 
+// We're measuring seconds as the most granular unit of time
+unsigned seconds = 0;
+
+void setup()
 {
   Serial.begin(9600);
   Serial.println("Slave");
@@ -16,35 +19,57 @@ void setup()
   byte digitPins[] = {10, 11, 12, 13};
   byte segmentPins[] = {9, A3, 3, 5, 6, 8, 7, 4};
 
-  bool resistorsOnSegments = true; 
+  bool resistorsOnSegments = true;
   bool updateWithDelaysIn = true;
-  byte hardwareConfig = COMMON_CATHODE; 
+  byte hardwareConfig = COMMON_CATHODE;
   sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
   sevseg.setBrightness(90);
 
   pinMode(BTN_PIN, INPUT_PULLUP);
   attachInterrupt(0,btn_pressed,FALLING);
-  
+
   Wire.begin(SLAVE_ADDR);
   Wire.onReceive(recvEvent);
+
+  // Initialize timer interrupt registers
+  cli();
+  TCCR1A = 0;
+  TCCR1B = 0;
+  OCR1A = 15625; // (16MHz/1024) * 1s
+
+  // Timer control register set Clock/1024 //
+  TCCR1B |= (1 << WGM12);
+  // TCCR1B |= (1 << CS10); // TODO: Uncomment for seconds
+  TCCR1B |= (1 << CS12);
+  TIMSK1  = (1 << OCIE1A);
+  sei();
 }
 
-void btn_pressed() 
+ISR(TIMER1_COMPA_vect) {
+  ++seconds;
+  Serial.print(seconds); Serial.print(" seconds\n");
+}
+
+void btn_pressed()
 {
-  
 }
 
-void recvEvent(int bytes) 
-{  
+void recvEvent(int bytes) {
   while(Wire.available()) {
     int cmd = Wire.read();
     Serial.print(cmd);         // print the character
   }
 }
 
-void loop() 
+unsigned make_display_value() {
+  unsigned minutes = seconds % 60;
+  unsigned hours = (seconds/60) % 60;
+
+  return (hours * 100) + (minutes);
+}
+
+void loop()
 {
-  sevseg.setNumber(0, 2);
+  sevseg.setNumber(make_display_value(), 2);
   sevseg.refreshDisplay();
-  delay(100);
 }
